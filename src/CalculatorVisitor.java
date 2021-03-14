af499a5aec27f40b7438c2c5e03f172ce9ad4d9b
Cata <alex_catalin1700@yahoo.com>
2021-03-14 18:56:26 +0100

    Cata: V1.2 WTF

--- src/CalculatorVisitor.java
+++ src/CalculatorVisitor.java
@@ -1,28 +1,65 @@
-public class CalculatorVisitor implements Calculator, Visitor
-{
+public class CalculatorVisitor implements Calculator, Visitor {
 
-  private LinkedStack tokenStack;
+    private LinkedStack<Token> tokenStack;
 
-  @Override public int getResult() throws Exception
-  {
-    throw new Exception("MalformedExpressionException");
-  };
+    @Override
+    public int getResult() throws Exception {
+        throw new Exception("MalformedExpressionException");
+    }
 
-  @Override public void visit(Operand operand)
-  {
+    ;
 
-  }
+    @Override
+    public void visit(Operand operand) {
+        tokenStack.push(operand);
+    }
 
-  @Override public void visit(Operator operator)
-  {
+    @Override
+    public void visit(Operator operator) {
+        try {
 
-  }
 
-  private void pushOperand(Operand operand){
 
-  }
-  private void performOperation(Operator operator) throws Exception{
-    
-    throw new Exception("MalformedExpressionException");
-  }
+            tokenStack.push(new Operand(performOperation(leftElm, rightElm, operator.getOperation())));
+        } catch (Exception e) {
+            if (tokenStack.isEmpty()) System.out.println("Exception regarding: " + operator);
+        }
+    }
+
+    private void pushOperand(Operand operand) {
+
+    }
+
+    private void performOperation(Operator operator) throws Exception {
+
+      int leftElm = 0;
+      int rightElm = 0;
+
+      if (!tokenStack.isEmpty()) {
+        rightElm = ((Operand) tokenStack.pop()).getValue();
+
+      }
+      if (!tokenStack.isEmpty()) {
+        leftElm = ((Operand) tokenStack.pop()).getValue();
+      }
+
+
+      //TODO: WTF
+      switch (operator) {
+        case :
+          return leftOperand + rightOperand;
+        case SUBTRACT:
+          return leftOperand - rightOperand;
+        case MULTIPLY:
+          return leftOperand * rightOperand;
+        case DIVIDE:
+          if(rightOperand == 0)
+            throw new Exception("Cannot divide by 0");
+          return leftOperand / rightOperand;
+        default:
+          return 0;
+      }
+
+        throw new Exception("MalformedExpressionException");
+    }
 }
--- src/Operand.java
+++ src/Operand.java
@@ -2,6 +2,11 @@ public class Operand extends Token {
 
     private int value;
 
+    public Operand(int value)
+    {
+        this.value = value;
+    }
+
     @Override
     public void accept(CalculatorVisitor visitor) {
         visitor.visit(this);
