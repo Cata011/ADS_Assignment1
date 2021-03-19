81259c5a2f5d0240d729cf9495f64e5725ce8428
Artis <mundurs.artis@gmail.com>
2021-03-19 19:46:40 +0100

    Added Tests

--- src/tests/LinkedListTest.java
+++ src/tests/LinkedListTest.java
@@ -1,24 +1,118 @@
 package tests;
 
+import customExceptions.EmptyListException;
+import org.junit.jupiter.api.Assertions;
+import stack.LinkedList;
+import stack.Node;
+
 class LinkedListTest {
+
+    private LinkedList<Integer> list;
+    private Node<Integer> head;
+    private int elm1;
+    private int elm2;
+    private int elm3;
+    private int elm4;
+
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        this.list = new LinkedList<>();
+        this.elm1 = 1;
+        this.elm2 = 2;
+        this.elm3 = 3;
+        this.elm4 = 4;
+        this.head = new Node<>(elm1);
     }
 
     @org.junit.jupiter.api.Test
     void isEmpty() {
+        Assertions.assertTrue(list.isEmpty());
     }
 
     @org.junit.jupiter.api.Test
-    void size() {
+    void isEmptyFalse() {
+        list.addToFront(elm1);
+        Assertions.assertFalse(list.isEmpty());
+    }
+
+    @org.junit.jupiter.api.Test
+    void sizeZero() {
+        Assertions.assertEquals(list.size(), 0);
+    }
+
+    @org.junit.jupiter.api.Test
+    void sizeOne() {
+        list.addToFront(elm1);
+        Assertions.assertEquals(list.size(), 1);
+    }
+
+    @org.junit.jupiter.api.Test
+    void sizeMany() {
+        list.addToFront(elm1);
+        list.addToFront(elm2);
+        Assertions.assertEquals(list.size(), 2);
     }
 
     @org.junit.jupiter.api.Test
     void addToFront() {
+        Assertions.assertEquals(list.size(), 0);
+        list.addToFront(elm1);
+        Assertions.assertEquals(list.size(), 1);
+        Assertions.assertFalse(list.isEmpty());
     }
 
     @org.junit.jupiter.api.Test
-    void removeFirst() {
+    void addToFrontMany() {
+        Assertions.assertEquals(list.size(), 0);
+        list.addToFront(elm1);
+        list.addToFront(elm2);
+        list.addToFront(elm3);
+        list.addToFront(elm4);
+        Assertions.assertEquals(list.size(), 4);
+        Assertions.assertFalse(list.isEmpty());
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeFirstEmpty() {
+        Assertions.assertThrows(EmptyListException.class, () ->
+                {
+                    list.removeFirst();
+                }
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeFirstOne() throws EmptyListException {
+        list.addToFront(elm1);
+        Assertions.assertEquals(list.size(), 1);
+        int nr = list.removeFirst();
+        Assertions.assertEquals(list.size(), 0);
+        Assertions.assertEquals(nr, elm1);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeFirstMany() throws EmptyListException {
+        list.addToFront(elm1);
+        list.addToFront(elm2);
+        list.addToFront(elm3);
+        list.addToFront(elm4);
+        Assertions.assertEquals(list.size(), 4);
+        int nr = list.removeFirst();
+        Assertions.assertEquals(list.size(), 3);
+        Assertions.assertEquals(nr, elm4);
+
+        int nr1 = list.removeFirst();
+        Assertions.assertEquals(list.size(), 2);
+        Assertions.assertEquals(nr1, elm3);
+
+        int nr2 = list.removeFirst();
+        Assertions.assertEquals(list.size(), 1);
+        Assertions.assertEquals(nr2, elm2);
+
+        int nr3 = list.removeFirst();
+        Assertions.assertEquals(list.size(), 0);
+        Assertions.assertEquals(nr3, elm1);
     }
 }
--- src/tests/LinkedStackTest.java
+++ src/tests/LinkedStackTest.java
@@ -1,20 +1,89 @@
 package tests;
+import customExceptions.EmptyStackException;
+import org.junit.jupiter.api.Assertions;
+import stack.LinkedStack;
+import stack.Node;
 
 class LinkedStackTest {
 
+    private LinkedStack<Integer> list;
+    private int elm1;
+    private int elm2;
+    private int elm3;
+    private int elm4;
+
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        this.list = new LinkedStack<>();
+        this.elm1 = 1;
+        this.elm2 = 2;
+        this.elm3 = 3;
+        this.elm4 = 4;
     }
 
     @org.junit.jupiter.api.Test
     void isEmpty() {
+        Assertions.assertTrue(list.isEmpty());
+    }
+
+    @org.junit.jupiter.api.Test
+    void isEmptyFalse() {
+        list.push(elm1);
+        Assertions.assertFalse(list.isEmpty());
     }
 
     @org.junit.jupiter.api.Test
     void push() {
+        Assertions.assertTrue(list.isEmpty());
+        list.push(elm1);
+        Assertions.assertFalse(list.isEmpty());
     }
 
     @org.junit.jupiter.api.Test
-    void pop() {
+    void pushMore() {
+        Assertions.assertTrue(list.isEmpty());
+        list.push(elm1);
+        list.push(elm2);
+        list.push(elm3);
+        Assertions.assertFalse(list.isEmpty());
+    }
+
+    @org.junit.jupiter.api.Test
+    void popNull() throws EmptyStackException {
+        Assertions.assertThrows(EmptyStackException.class, () ->
+        {
+            Assertions.assertTrue(list.isEmpty());
+            list.pop();
+        });
+    }
+
+    @org.junit.jupiter.api.Test
+    void pop() throws EmptyStackException {
+        list.push(elm3);
+        Assertions.assertFalse(list.isEmpty());
+        Integer integer = list.pop();
+        Assertions.assertEquals(integer, 3);
+        Assertions.assertTrue(list.isEmpty());
+    }
+
+    @org.junit.jupiter.api.Test
+    void popMore() throws EmptyStackException {
+        list.push(elm1);
+        list.push(elm2);
+        list.push(elm3);
+        list.push(elm4);
+        Assertions.assertFalse(list.isEmpty());
+        Integer integer = list.pop();
+        Assertions.assertEquals(integer, 4);
+        Assertions.assertFalse(list.isEmpty());
+        Integer integer1 = list.pop();
+        Assertions.assertEquals(integer1, 3);
+        Assertions.assertFalse(list.isEmpty());
+        Integer integer2 = list.pop();
+        Assertions.assertEquals(integer2, 2);
+        Assertions.assertFalse(list.isEmpty());
+        Integer integer3 = list.pop();
+        Assertions.assertEquals(integer3, 1);
+        Assertions.assertTrue(list.isEmpty());
     }
 }
