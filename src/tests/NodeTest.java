b1a6d3cd0794b7051253be7c82634cbc59345876
Artis <mundurs.artis@gmail.com>
2021-03-19 19:46:06 +0100

    Update NodeTest.java

--- src/tests/NodeTest.java
+++ src/tests/NodeTest.java
@@ -1,29 +1,61 @@
 package tests;
 
+import org.junit.jupiter.api.Assertions;
 import stack.Node;
 
 class NodeTest<T> {
 
-    private T data;
-    private Node<T> next;
+    private Node<Integer> node;
+    private int elm1;
+    private int elm2;
+    private int elm3;
+    private int elm4;
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        this.elm1 = 1;
+        this.elm2 = 2;
+        this.elm3 = 3;
+        this.elm4 = 4;
+        this.node = new Node<>(elm1);
     }
 
     @org.junit.jupiter.api.Test
     void setData() {
+        Assertions.assertEquals(node.getData(), 1);
+        node.setData(elm4);
+        Assertions.assertEquals(node.getData(), 4);
+    }
+
+    @org.junit.jupiter.api.Test
+    void setDataNull() {
+        Assertions.assertEquals(node.getData(), 1);
+        node.setData(null);
+        Assertions.assertNull(node.getData());
     }
 
     @org.junit.jupiter.api.Test
     void getData() {
+        Assertions.assertEquals(node.getData(),1);
+        Assertions.assertNotEquals(node.getData(), 2);
     }
 
     @org.junit.jupiter.api.Test
     void setNext() {
+        Node<Integer> otherNode = new Node<>(elm4);
+        node.setNext(otherNode);
+        Assertions.assertEquals(node.getNext().getData(), 4);
+    }
+
+    void setNextNull() {
+        node.setNext(null);
+        Assertions.assertNull(node.getNext().getData());
     }
 
     @org.junit.jupiter.api.Test
     void getNext() {
+        Node<Integer> otherNode = new Node<>(elm3);
+        node.setNext(otherNode);
+        Assertions.assertEquals(node.getNext().getData(), 3);
     }
 }
